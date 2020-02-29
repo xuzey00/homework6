c1d1adadf031518f6f6bc9c1ffaad5c847b5dd58
Administrator <xuzey00@gmail.com>
2020-02-29 12:18:31 -0800

    rough attempts

--- src/MyMiniSearchEngine.java
+++ src/MyMiniSearchEngine.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 
 public class MyMiniSearchEngine {
     // default solution. OK to change.
@@ -19,6 +20,13 @@ public class MyMiniSearchEngine {
     // assume documents only contain alphabetical words separated by white spaces.
     private void index(List<String> texts) {
         //homework
+        Map<Integer,String> map = new HashMap<Integer, String>();
+        int i = 0;
+        for(String text : texts){
+            map.put(i,text);
+            i++;
+        }
+        System.out.println(map);
     }
 
     // search(key) return all the document ids where the given key phrase appears.
