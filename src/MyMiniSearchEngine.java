d15dde518d837c09746774d73657461bdfff7ea5
Administrator <xuzey00@gmail.com>
2020-03-05 20:37:06 -0800

    close to be done

--- src/MyMiniSearchEngine.java
+++ src/MyMiniSearchEngine.java
@@ -25,7 +25,7 @@ public class MyMiniSearchEngine {
 
         @Override
         public String toString(){
-            return "{doc"+ docNum + ", location" + loc.toString() + "}";
+            return "{doc["+ docNum + "], location" + loc.toString() + "}"+"\n";
         }
     }
     private Map<String, List<wordIndex>> indexes;
@@ -45,29 +45,38 @@ public class MyMiniSearchEngine {
         indexes = new HashMap<String, List<wordIndex>>();
         int docNum = 0;
         for(String text : texts){
-            String[] words = text.split(" "); //doc 0:"hello", "world"  doc 1:"hello"  doc 2:"world"  ...
+            //doc 0:"hello", "world"  doc 1:"hello"  doc 2:"world"  ...
+            //split the text
+            String[] words = text.split(" ");
             for(int x = 0; x<words.length; x++){
                 List<wordIndex> docList;
                 wordIndex numList;
+                //set the word to lower case
                 String word = words[x].toLowerCase();
                 if(!indexes.containsKey(word)){
+                    //if word is not in the map, create new list
                     docList = new ArrayList<>();
                     indexes.put(word, docList);
-                }else if(indexes.get(word).get(docNum) == null){
+                }else{
+                    //if already in map, get the word list
                     docList = indexes.get(word);
                 }
-                if (docList.IsEmpty() || docList.get(docList.size()-1).getDocNum() != docNum) {
+                if (docList.isEmpty() || docList.get(docList.size()-1).getDocNum() != docNum) {
+                    //word in a new document, add a new doc num list to the doc list
                     numList = new wordIndex(docNum);
                     docList.add(numList);
                 }
                 else {
+                    //get the current doc num list from the end of the docList
                     numList = docList.get(docList.size() - 1);
                 }
-
+                //add the word to numList
+                numList.AddLocation(x);
             }
+            //go to next doc
             docNum++;
         }
-        System.out.println(indexes);
+        //System.out.println(indexes);
         //System.out.println(indexes.get("hello"));
     }
 
@@ -78,7 +87,22 @@ public class MyMiniSearchEngine {
         // homework
         // goes through the map, use map.containKey(keyPhrase) to locate the value
         // return with map.get()
-        List<List<Integer>> result = new ArrayList<>();
-        return new ArrayList<>(); // place holder
+        String[] keys = keyPhrase.split(" ");
+        Map<Integer,List<Integer>> finalResult = new HashMap<>();
+        List<wordIndex> result;
+        ArrayList<Integer> location;
+        for(int i = 0; i<keys.length;i++) {
+            if (indexes.containsKey(keys[i])) {
+                //if map contains key
+                result = indexes.get(keys[i]);
+                int num = result.get(i).getDocNum();
+                location = result.get(i).getLocation();
+                finalResult.put(num,location);
+                System.out.println("key:["+keys[i]+"],result["+result+"]"+num + location);
+            }
+        }
+        System.out.println(finalResult);
+        // return empty list, no words being found
+        return Collections.emptyList();
     }
 }
